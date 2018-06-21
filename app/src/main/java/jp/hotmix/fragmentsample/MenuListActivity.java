38911929390aafc76c89a0bcadc043e85647cd25
hotmix <dai@hotmix.jp>
2018-06-21 09:38:07 +0900

    add menu_thanks_fragment

--- app/src/main/java/jp/hotmix/fragmentsample/MenuListActivity.java
+++ app/src/main/java/jp/hotmix/fragmentsample/MenuListActivity.java
@@ -2,12 +2,50 @@ package jp.hotmix.fragmentsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MenuListActivity extends AppCompatActivity {
+    private final static String TAG = "MenuActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
+
+        Log.i(TAG, "onCreate: called");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: called");
+    }
+
+
 }
--- app/src/main/java/jp/hotmix/fragmentsample/MenuListFragment.java
+++ app/src/main/java/jp/hotmix/fragmentsample/MenuListFragment.java
@@ -2,12 +2,18 @@ package jp.hotmix.fragmentsample;
 
 
 import android.app.Activity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -23,6 +29,8 @@ import java.util.Map;
 public class MenuListFragment extends Fragment {
 
     private Activity _parentActivity;
+    private final static String TAG = "MenuFragment";
+    private boolean _isLayoutXLarge = true;
 
 
     public MenuListFragment() {
@@ -33,11 +41,14 @@ public class MenuListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         _parentActivity = getActivity();
+        Log.i(TAG, "onCreate: called");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Log.i(TAG, "onCreateView: called");
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_menu_list, container, false);
         ListView lvMenu = view.findViewById(R.id.lvMenu);
@@ -49,8 +60,69 @@ public class MenuListFragment extends Fragment {
         SimpleAdapter adapter = new SimpleAdapter(_parentActivity, menuList, android.R.layout.simple_list_item_2, from, to);
 
         lvMenu.setAdapter(adapter);
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
 
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach: called");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Log.i(TAG, "onActivityCreated: called");
+
+        View menuThanksFrame = _parentActivity.findViewById(R.id.menuThanksFrame);
+        if (menuThanksFrame == null){
+            _isLayoutXLarge = false;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: called");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "onDestroyView: called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: called");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach: called");
     }
 
     private List<Map<String, String>> createTeishokuList(){
@@ -111,6 +183,37 @@ public class MenuListFragment extends Fragment {
         menuList.add(menu);
 
         return menuList;
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Map<String, String> item = (Map<String, String>)parent.getItemAtPosition(position);
+
+            String menuName = item.get("name");
+            String menuPrice = item.get("price");
+
+            Bundle bundle = new Bundle();
+            bundle.putString("menuName", menuName);
+            bundle.putString("menuPrice", menuPrice);
+
+            if (_isLayoutXLarge) {
+                FragmentManager manager = getFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                MenuThanksFragment menuThanksFragment = new MenuThanksFragment();
+                menuThanksFragment.setArguments(bundle);
+                transaction.replace(R.id.menuThanksFrame, menuThanksFragment);
+                transaction.commit();
+            } else {
+                Intent intent = new Intent(_parentActivity, MenuThanksActivity.class);
+
+                intent.putExtra("menuName", menuName);
+                intent.putExtra("menuPrice", menuPrice);
+
+                startActivity(intent);
+            }
+        }
     }
 
 
